bc9bf4bbeefe57d142b782840d2c49509b150c3b
zspajich <zspajich@gmail.com>
2017-10-28 07:19:08 +0200

    Adding varibles to Response

--- src/AlfredAutosuggest/ScriptFilterResponse.php
+++ src/AlfredAutosuggest/ScriptFilterResponse.php
@@ -7,12 +7,18 @@ class ScriptFilterResponse
 {
 
     protected $items = [];
+    protected $variables = [];
 
     public function addItem(ScriptFilterItem $item)
     {
         $this->items[] = $item;
     }
 
+    public function addVariable(string $name, $value)
+    {
+        $this->variables[$name] = $value;
+    }
+    
     public function toJson(): string
     {
         return json_encode([
